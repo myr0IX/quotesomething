c6ea96bbff6d85a43624c28cdaa07c3e27e78507
Mathys Cassagne <macassag@z3r2p4.42lyon.fr>
2024-08-19 13:30:12 +0200

    first implementation api, can't parse

--- src/App.js
+++ src/App.js
@@ -9,28 +9,38 @@ function Button (prop) {
 
 export default function App() {
   return (
-    <div className="App">
-		<Card title="Theme">ceci est la desciption : Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit gloriosam.</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-		<Card title="Theme">Theme</Card>
-    </div>
+	<div>
+		<div className="App">
+			<Card title="Theme">ceci est la desciption : Sed laeditur hic coetuum magnificus splendor levitate paucorum incondita, ubi nati sunt non reputantium, sed tamquam indulta licentia vitiis ad errores lapsorum ac lasciviam. ut enim Simonides lyricus docet, beate perfecta ratione vieturo ante alia patriam esse convenit gloriosam.</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+			<Card title="Theme">Theme</Card>
+		</div>
+	</div>
   );
 }
 
 function Card(props) {
+	async function getapi()
+	{
+	  const response = await fetch("https://api.quotable.io/quotes/random");
+	  var data = await response.json();
+	  console.log(data);
+	  console.log(typeof data.content);
+	}
 	return (
-	  <div className="Card br">
+	  <div>
+		<button className="Card Btn br" onClick={getapi}>
 		  <h3 className='Theme Case br'>{props.title}</h3>
-		  <p>{props.children}</p>
-		  <Button>Generate</Button>
+		  <p className='Case br'>{props.children}</p>
+		</button>
 	  </div>
 	);
   }
